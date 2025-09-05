b5533e9efc721812cc34ea471ff5adc40683e5d8
Michael <michael@ITAD_platform.com>
2025-09-05 12:53:05 -0400

    error checking for LogFinder

--- src/Utilities/LogFinder.py
+++ src/Utilities/LogFinder.py
@@ -1,4 +1,4 @@
-import pathlib
+import pathlib,logging
 
 class LogFinder():
     def __init__(self):
@@ -12,6 +12,24 @@ class LogFinder():
             print("Multiple XML logs detected, select one")
             for i,log in enumerate(xml_logs):
                 print("{} - {}".format(i,log.name))
-            selection = int(input(">>"))
+            
+            selection_text = selection = input(">>")
+
+            try:
+                selection = int(selection_text)
+            except:
+                logging.info("FAILED TO CONVERT TO INT")
+                print("INPUT FAILED TO BE CONVERTED TO NUMBER")
+                print(f"INPUT: {selection_text}")
+                
+                xml_logs.sort(key=lambda x: x.stat().st_ctime, reverse=True)
+                selection = 0
+                print(f"USING DEFAULT: {xml_logs[selection].name}")
+                
         uuid = xml_logs[selection].name.split(".")[0]
         return uuid
+
+if __name__ == "__main__":
+    lg = LogFinder()
+    uuid = lg.find_uuid()
+    print(uuid)
