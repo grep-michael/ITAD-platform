5f270679c4366cf081ad3a804f286e352b3f00f1
Michael <michael@ITAD_platform.com>
2025-12-04 08:54:11 -0500

    keyboard test only triggers on letters and numbers now

--- src/HardwareTests/Controllers/KeyboardTestController.py
+++ src/HardwareTests/Controllers/KeyboardTestController.py
@@ -11,19 +11,24 @@ class KeyboardTestController(ITADController):
         self.element = element
         self.parent = parent
         self.view:KeyboardTestView
-        self.unpressed_keys = [Qt.Key_Escape, Qt.Key_Print, Qt.Key_QuoteLeft, 
-                               Qt.Key_1, Qt.Key_2, Qt.Key_3, Qt.Key_4, Qt.Key_5, 
-                               Qt.Key_6, Qt.Key_7, Qt.Key_8, Qt.Key_9, Qt.Key_0, 
-                               Qt.Key_Minus, Qt.Key_Equal, Qt.Key_Backspace, Qt.Key_Tab, 
-                               Qt.Key_Q, Qt.Key_W, Qt.Key_E, Qt.Key_R, Qt.Key_T, Qt.Key_Y, 
-                               Qt.Key_U, Qt.Key_I, Qt.Key_O, Qt.Key_P, Qt.Key_BracketLeft, 
-                               Qt.Key_BracketRight, Qt.Key_Backslash, Qt.Key_CapsLock, 
-                               Qt.Key_A, Qt.Key_S, Qt.Key_D, Qt.Key_F, Qt.Key_G, Qt.Key_H, 
-                               Qt.Key_J, Qt.Key_K, Qt.Key_L, Qt.Key_Semicolon, Qt.Key_Apostrophe, 
-                               Qt.Key_Return, "L-Shift", Qt.Key_Z, Qt.Key_X, Qt.Key_C, Qt.Key_V, 
-                               Qt.Key_B, Qt.Key_N, Qt.Key_M, Qt.Key_Comma, Qt.Key_Period, 
-                               Qt.Key_Slash, "R-Shift", "L-Ctrl", "L-Alt", Qt.Key_Space, 
-                               "R-Alt", Qt.Key_Menu, "R-Ctrl"]
+        self.unpressed_keys = [
+                            #Qt.Key_Escape, Qt.Key_Print, Qt.Key_QuoteLeft, 
+                            Qt.Key_1, Qt.Key_2, Qt.Key_3, Qt.Key_4, Qt.Key_5, 
+                            Qt.Key_6, Qt.Key_7, Qt.Key_8, Qt.Key_9, Qt.Key_0, 
+                            #Qt.Key_Minus, Qt.Key_Equal, Qt.Key_Backspace, Qt.Key_Tab, 
+                            Qt.Key_Q, Qt.Key_W, Qt.Key_E, Qt.Key_R, Qt.Key_T, Qt.Key_Y, 
+                            Qt.Key_U, Qt.Key_I, Qt.Key_O, Qt.Key_P, Qt.Key_Z, Qt.Key_X, 
+                            Qt.Key_A, Qt.Key_S, Qt.Key_D, Qt.Key_F, Qt.Key_G, Qt.Key_H, 
+                            Qt.Key_J, Qt.Key_K, Qt.Key_L, Qt.Key_B, Qt.Key_N, Qt.Key_M,
+                            Qt.Key_C, Qt.Key_V,
+                             
+                            #Qt.Key_BracketRight, Qt.Key_Backslash, Qt.Key_CapsLock, 
+                            #Qt.Key_Semicolon, Qt.Key_Apostrophe, 
+                            #Qt.Key_Return, "L-Shift",  
+                            #Qt.Key_Comma, Qt.Key_Period, Qt.Key_BracketLeft,
+                            #Qt.Key_Slash, "R-Shift", "L-Ctrl", "L-Alt", Qt.Key_Space, 
+                            #"R-Alt", "R-Ctrl"
+                            ]
     
     
 
