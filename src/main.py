80e484aecf776908bb28ac91b4a3360906bfab8f
Michael <michael@ITAD_platform.com>
2026-07-07 16:11:49 -0400

    fuck

--- src/main.py
+++ src/main.py
@@ -67,14 +67,14 @@ fields = []
 storageCount = len(root.findall(".//Storage"))
 if storageCount<2:
     msg += f"Storage Count is incorrect\n"
-    fields += {"name":"Storage Count","value":f"{storageCount}"}
+    fields.append({"name":"Storage Count","value":f"{storageCount}"})
 
 networkCount = len(root.findall(".//Network"))
 if networkCount < 1:
     msg += f"No Network interfaces\n"
 
 
-if len(fields) > 0 and msg != "":
+if len(fields) > 0 or msg != "":
     SendDiscordError(f"{serial} Errors",msg,fields)
 
 
