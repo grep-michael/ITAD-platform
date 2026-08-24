53f957a6925814ebf5fcc07574ac48241b4a411a
Michael <michael@ITAD_platform.com>
2026-08-24 14:05:33 -0400

    better error logging

--- src/Pipeline.py
+++ src/Pipeline.py
@@ -17,16 +17,20 @@ class Pipeline:
                 logging.error("FatalError: %s",e)
                 SendDiscordError(ctx.serial, f"{step.name}: {e}")
                 AuditLogPost("Errored",FatalError=f"{step.name}: {e}")
+                os._exit(-1)
                 break
             except Exception as e:
                 logging.error("Unexpected error: %s",e)
                 SendDiscordError(ctx.serial, f"{step.name}: {e}")
                 AuditLogPost("Errored",Exception=f"{step.name}: {e}")
         if ctx.errors:
+            errors = {}
+            for err in ctx.errors:
+                errors[err.Message] = [asdict(f) for f in err.Fields]
             msg = "\n".join([err.Message for err in ctx.errors])
             fields = [asdict(f) for err in ctx.errors for f in err.Fields]
             SendDiscordError(f"{ctx.serial} Errors", msg, fields)
-            AuditLogPost("Errored",Errors=fields)
+            AuditLogPost("Errored",Errors=errors)
         else:
             SendDiscordSuccess(f"{ctx.serial} Success", "No Errors Detected")
             print(ctx.Commodities)
